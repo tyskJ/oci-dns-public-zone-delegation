f4ceac5a627e3f5e22357f6bfc0bed6e40d3e6b6
tyskJ <193552101+tyskJ@users.noreply.github.com>
2025-12-11 11:44:06 +0900

    :bug: 循環参照になるため削除

--- env/compartments.tf
+++ env/compartments.tf
@@ -6,8 +6,4 @@ resource "oci_identity_compartment" "dns_public_zone_delegation" {
   name           = "dns-public-zone-delegation"
   description    = "For DNS Public Zone Delegation"
   enable_delete  = true
-  defined_tags = {
-    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_env.tag_definition_name)                = "prd"
-    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_managedbyterraform.tag_definition_name) = "true"
-  }
 }
